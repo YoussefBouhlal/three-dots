6211170c1c8c18cdac3e552b8a089ed28b0e25b2
youssef bouhlal <youssef.bouhlal@webdeclic.com>
2023-09-22 00:14:35 +0100

    first commit

---
+++ build/js/app.js
@@ -0,0 +1,1 @@
+var Global;!function(){"use strict";var e={r:function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},o={};e.r(o),console.log("hello"),Global=o}();
